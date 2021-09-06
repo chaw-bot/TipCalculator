a3676dbd317c77da7b33acfe7cb54f7d5b9a78e5
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-09-06 14:55:39 +0200

    Function for tip percentages to calculate tip per person

--- tip.js
+++ tip.js
@@ -2,28 +2,35 @@
 let bill = document.querySelector('#bill');
 let people = document.querySelector('#numOfPeople');
 let custom = document.querySelector('#custom');
+let tipAmount = 0;
 
 const reset = document.querySelector('.reset');
       tip = document.querySelector('.tip');
       five = document.querySelector('.five');
       ten = document.querySelector('.ten');
       fifteen = document.querySelector('.fifteen');
-      twentyfive = document.querySelector('.twentyFive');
+      twentyFive = document.querySelector('.twentyFive');
       fifty = document.querySelector('.fifty');
-      
+  
     
-// caluculate tip
-function calcTip() {
-  // tip %
-  five.value = 5;
-  // ten.value = 10;
-  // fifteen.value = 15;
-  // twentyFive.value = 25;
+// get custom tip
+function getTipCustom() {
+  tipAmount = parseInt(custom.value, 10);
 }
+
+// get tip
+document.querySelector('.tip-container').addEventListener('click', event => {
+  if (event.target !== five && event.target !== ten && event.target !== fifteen && event.target !== twentyFive && event.target !== fifty) {
+    return
+  }
+  tipAmount = event.target.value 
+});
 
 // calculate bill per person
 function calcBill() {
   // get the bill
+
+  console.log(tipAmount);
   let totalBill = bill.value;
 
   // number of people
@@ -31,9 +38,8 @@ function calcBill() {
 
   let total = totalBill / totalPeople;
 
-  
   // tip per person
-  document.querySelector('#amount').value = (total * (five.value / 100));
+  document.querySelector('#amount').value = (total * (tipAmount / 100));
 
   // show total per person
   document.querySelector('#total').value = total;
@@ -41,4 +47,4 @@ function calcBill() {
 
 people.addEventListener('keyup', calcBill);
 
-tip.addEventListener('click', calcTip);
+custom.addEventListener('keyup', getTipCustom);
